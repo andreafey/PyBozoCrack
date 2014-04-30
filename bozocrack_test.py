6552d7b81b911950e224f0e3167634926e759a5b
Andrea Fey <andrea.github@mailnull.com>
2014-04-30 12:05:50 -0400

    mult python versions

--- bozocrack_test.py
+++ bozocrack_test.py
@@ -9,6 +9,3 @@ def test_crack_single_hash():
     result = bozocrack.crack_single_hash('fcf1eed8596699624167416a1e7e122e')
     expected = 'octopus'
     assert expected == result
-
-def test_fail():
-    assert 4 == 9
